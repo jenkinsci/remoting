37248f183cc2e305d471183c027bfa17404beb93
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-03-20 10:59:46 -0700

    added test code for measuring throughput.
    Use this with a delay injection in  the loop back interface:
    
      $ sudo tc qdisc add dev lo root netem delay 100ms
    
    To reset the delay and bring it back to normal:
    
      $ sudo tc qdisc del dev lo root
    
    Query current status:
    
      $ sudo tc -s qdisc ls dev lo

---
+++ src/test/java/hudson/remoting/throughput/Receiver.java
@@ -0,0 +1,30 @@
+package hudson.remoting.throughput;
+
+import hudson.remoting.Channel;
+import hudson.remoting.SocketInputStream;
+import hudson.remoting.SocketOutputStream;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+
+/**
+ * Accepts a channel one at a time.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class Receiver {
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(PORT);
+        while (true) {
+            System.out.println("Ready");
+            Socket s = ss.accept();
+            System.out.println("Accepted");
+            Channel ch = new Channel("bogus", Executors.newCachedThreadPool(),
+                    new SocketInputStream(s),new SocketOutputStream(s));
+            ch.join();
+            s.close();
+        }
+    }
+    public static final int PORT = 9532;
+}
