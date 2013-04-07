e490e0d60e436bae471e4d3c2603976efbd63d4e
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-04-07 06:43:12 -0700

    What is this test doing?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,6 +37,13 @@ public class Main {
      */
 
     public static void main(String[] args) throws Exception {
+        testProcessSelection();
+    }
+
+    /**
+     * Tests NIO with pipe to child processes.
+     */
+    private static void testProcessSelection() throws Exception {
         SelectableFileChannelFactory f = new SelectableFileChannelFactory();
         Selector sel = Selector.open();
 
