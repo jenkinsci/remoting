73899f44d1d68d8ab70d00bbd8d53e3d249fda0e
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-05-17 10:17:43 +0200

    Added an annotation to make a remote all asynchronous

---
+++ src/main/java/hudson/remoting/Asynchronous.java
@@ -0,0 +1,43 @@
+package hudson.remoting;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+/**
+ * Used on a method in a remotable exported interface to designate
+ * that the call is made asynchronously. The call will be issued,
+ * but the caller will return without waiting for the return value
+ * to come back from the other side.
+ *
+ * The signature of the method must return void.
+ *
+ * <pre>
+ * interface Foo {
+ *     void bar();
+ *     &#64;Asynchronous
+ *     void zot();
+ * }
+ *
+ * Foo foo = getSomeRemoteReferenceToFoo();
+ * // this invocation calls a remote method, wait for that to complete,
+ * // then return.
+ * foo.bar();
+ * // this invocation returns immediately after the request to execute a remote method
+ * // is sent to the other side. There's no ordering guarantee as to when
+ * // this method actually gets executed. For example, if you invoke two async
+ * // calls, they may execute in the reverse order.
+ * foo.zot();
+ *
+ *
+ * @see Channel#callAsync(Callable)
+ * @author Kohsuke Kawaguchi
+ */
+@Documented
+@Target(METHOD)
+@Retention(RUNTIME)
+public @interface Asynchronous {
+}
