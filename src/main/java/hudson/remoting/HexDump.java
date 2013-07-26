aae9b457c15f88b7a8607978177f1673db2ec2ff
Daniel Beck <daniel-beck@github.com>
2013-07-26 21:17:02 +0200

    [JENKINS-8856] Nicer logging of ASCII text.
    It's no longer a 'hex' dump, but much more readable for all known error cases (JVM dying and writing to standard out).

--- src/main/java/hudson/remoting/HexDump.java
+++ src/main/java/hudson/remoting/HexDump.java
@@ -11,18 +11,34 @@ public class HexDump {
     }
     public static String toHex(byte[] buf, int start, int len) {
         StringBuilder r = new StringBuilder(len*2);
+        boolean inText = false;
         for (int i=0; i<len; i++) {
-            if (i > 0) {
-                r.append(' ');
-            }
             byte b = buf[start+i];
             if (b >= 0x20 && b <= 0x7e) {
-                r.append('\'').append((char) b).append('\'');
+                if (!inText) {
+                    inText = true;
+                    r.append('\'');
+                }
+                r.append((char) b);
             } else {
+                if (inText) {
+                    r.append("' ");
+                    inText = false;
+                }
                 r.append("0x");
                 r.append(CODE.charAt((b>>4)&15));
                 r.append(CODE.charAt(b&15));
+                if (i < len - 1) {
+                    if (b == 10) {
+                        r.append('\n');
+                    } else {
+                        r.append(' ');
+                    }
+                }
             }
+        }
+        if (inText) {
+            r.append('\'');
         }
         return r.toString();
     }
