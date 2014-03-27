60a23067c7961b183b0463ff2c42cd76897fbe0f
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-27 10:31:35 -0700

    adding another test

--- src/test/java/hudson/remoting/ProxyWriterTest.java
+++ src/test/java/hudson/remoting/ProxyWriterTest.java
@@ -1,5 +1,6 @@
 package hudson.remoting;
 
+import junit.framework.Test;
 import org.jvnet.hudson.test.Bug;
 
 import java.io.ByteArrayOutputStream;
@@ -57,7 +58,7 @@ public class ProxyWriterTest extends RmiTestBase implements Serializable {
         // induce a GC. There's no good reliable way to do this,
         // and if GC doesn't happen within this loop, the test can pass
         // even when the underlying problem exists.
-        for (int i=0; i<50; i++) {
+        for (int i=0; i<30; i++) {
             logRecorder.flush();
             assertTrue("There shouldn't be any errors: " + log.toString(), log.size() == 0);
 
@@ -73,7 +74,32 @@ public class ProxyWriterTest extends RmiTestBase implements Serializable {
         assertFalse(streamClosed);
     }
 
+
+    /**
+     * Basic test of {@link RemoteWriter}/{@link ProxyWriter} test.
+     */
+    public void testWriteAndSync() throws InterruptedException, IOException {
+        StringWriter sw = new StringWriter();
+        final RemoteWriter w = new RemoteWriter(sw);
+
+        for (int i=0; i<16; i++) {
+            channel.call(new Callable<Void, IOException>() {
+                public Void call() throws IOException {
+                    w.write("1--",0,1);
+                    return null;
+                }
+            });
+            w.write("2");
+        }
+
+        assertEquals("12121212121212121212121212121212",sw.toString());
+    }
+
     private Object writeReplace() {
         return null;
+    }
+
+    public static Test suite() throws Exception {
+        return buildSuite(ProxyWriterTest.class);
     }
 }
