e65f7f7a87279d82f5ee67450cddc79083ab6e85
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-04-01 09:28:27 -0700

    Added adapter

---
+++ src/main/java/hudson/remoting/EngineListenerAdapter.java
@@ -0,0 +1,21 @@
+package hudson.remoting;
+
+/**
+ * Adapter class for {@link EngineListener} to shield subtypes from future callback additions.
+ *
+ * @author Kohsuke Kawaguchi
+ * @since 2.36
+ */
+public abstract class EngineListenerAdapter implements EngineListener {
+    public void status(String msg) {
+    }
+
+    public void status(String msg, Throwable t) {
+    }
+
+    public void error(Throwable t) {
+    }
+
+    public void onDisconnect() {
+    }
+}
