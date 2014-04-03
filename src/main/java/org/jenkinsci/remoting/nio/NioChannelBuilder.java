198fbd11d45e8fd08e089e528d1044ae29eb0bc6
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-04-03 16:54:51 -0700

    needs to return the 'self' type

--- src/main/java/org/jenkinsci/remoting/nio/NioChannelBuilder.java
+++ src/main/java/org/jenkinsci/remoting/nio/NioChannelBuilder.java
@@ -1,9 +1,13 @@
 package org.jenkinsci.remoting.nio;
 
+import hudson.remoting.Capability;
 import hudson.remoting.Channel;
+import hudson.remoting.Channel.Mode;
 import hudson.remoting.ChannelBuilder;
+import hudson.remoting.JarCache;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SocketChannel;
@@ -28,5 +32,36 @@ public class NioChannelBuilder extends ChannelBuilder {
     @Override
     public Channel build(Socket s) throws IOException {
         return build(s.getChannel());
+    }
+
+
+    @Override
+    public NioChannelBuilder withBaseLoader(ClassLoader base) {
+        return (NioChannelBuilder)super.withBaseLoader(base);
+    }
+
+    @Override
+    public NioChannelBuilder withMode(Mode mode) {
+        return (NioChannelBuilder)super.withMode(mode);
+    }
+
+    @Override
+    public NioChannelBuilder withCapability(Capability capability) {
+        return (NioChannelBuilder) super.withCapability(capability);
+    }
+
+    @Override
+    public NioChannelBuilder withHeaderStream(OutputStream header) {
+        return (NioChannelBuilder) super.withHeaderStream(header);
+    }
+
+    @Override
+    public NioChannelBuilder withRestricted(boolean restricted) {
+        return (NioChannelBuilder) super.withRestricted(restricted);
+    }
+
+    @Override
+    public NioChannelBuilder withJarCache(JarCache jarCache) {
+        return (NioChannelBuilder) super.withJarCache(jarCache);
     }
 }
