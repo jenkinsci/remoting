b6365ceabfeed2fd3a6fab4bdf0586bbadf4608e
Jesse Glick <jglick@cloudbees.com>
2014-09-23 17:13:36 -0400

    Fixed Javadoc to make it clear that you delegate to stem, not op.

--- src/main/java/hudson/remoting/CallableDecorator.java
+++ src/main/java/hudson/remoting/CallableDecorator.java
@@ -22,21 +22,21 @@ public abstract class CallableDecorator {
      * Unlike {@link #call(java.util.concurrent.Callable)}, this method provides an opportunity
      * to inspect the actual {@link Callable} object given to {@link Channel#call(Callable)}
      * from the other side, whereas {@link #call(java.util.concurrent.Callable)} only
-     * provides opaque blob that itself may wrap the actual user-given operations.
+     * provides an opaque blob that itself may wrap the actual user-given operations.
      *
      * @param op
-     *      The callable object given to {@link Channel#call(Callable)}
+     *      The original callable object given to {@link Channel#call(Callable)}.
      * @param stem
-     *      Computation that represents the invocation of 'op' as well as any additional decoration done by other
+     *      Computation that represents the invocation of {@code op} as well as any additional decoration done by other
      *      {@link CallableDecorator}s.
      * @return
      *      Returns the a decorated {@link Callable} that represents the decorated computation,
-     *      which normally executes some pre-processing, then delegate to the 'op', then perform some cleanup.
+     *      which normally executes some pre-processing, then delegates to the {@code stem}, then performs some cleanup.
      *
-     *      If there's nothing to filter, return 'op'.
+     *      If there's nothing to filter, return {@code stem}.
      * @throws RuntimeException
      *      Any exception thrown from this method will be propagated to the other side as if the execution of
-     *      the callable has failed with this exception.
+     *      the callable had failed with this exception.
      */
     public <V,T extends Throwable> Callable<V,T> userRequest(Callable<V,T> op, Callable<V,T> stem) {
         return stem;
