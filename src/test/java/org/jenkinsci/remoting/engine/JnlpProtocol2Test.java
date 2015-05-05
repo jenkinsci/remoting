1dc2fdfe31bd4559272c9aad39c45ccd7f222652
Akshay Dayal <akshay_abd@yahoo.com>
2015-05-05 15:38:07 -0700

    Minor tweak

--- src/test/java/org/jenkinsci/remoting/engine/JnlpProtocol2Test.java
+++ src/test/java/org/jenkinsci/remoting/engine/JnlpProtocol2Test.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
+ *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -81,7 +81,6 @@ public class JnlpProtocol2Test {
         ByteArrayOutputStream expectedPropertiesStream = new ByteArrayOutputStream();
         expectedProperties.store(expectedPropertiesStream, null);
 
-        mockStatic(EngineUtil.class);
         when(EngineUtil.readLine(mockBufferedInputStream)).thenReturn("error");
 
         assertEquals("error",
@@ -104,7 +103,6 @@ public class JnlpProtocol2Test {
         Properties responseProperties = new Properties();
         responseProperties.put(JnlpProtocol2.COOKIE_KEY, COOKIE);
 
-        mockStatic(EngineUtil.class);
         when(EngineUtil.readLine(mockBufferedInputStream))
                 .thenReturn(JnlpProtocol.GREETING_SUCCESS);
         when(EngineUtil.readResponseHeaders(mockBufferedInputStream))
@@ -144,7 +142,6 @@ public class JnlpProtocol2Test {
         Properties responseProperties2 = new Properties();
         responseProperties2.put(JnlpProtocol2.COOKIE_KEY, COOKIE2);
 
-        mockStatic(EngineUtil.class);
         when(EngineUtil.readLine(mockBufferedInputStream))
                 .thenReturn(JnlpProtocol.GREETING_SUCCESS);
         when(EngineUtil.readResponseHeaders(mockBufferedInputStream)).thenReturn(responseProperties)
