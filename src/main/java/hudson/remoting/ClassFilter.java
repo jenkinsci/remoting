7be1d819949870013c6397907d721317066bcb86
James Nord <teilo@teilo.net>
2016-06-09 11:02:02 +0100

    Move the loading of the pattern override into the try block

--- src/main/java/hudson/remoting/ClassFilter.java
+++ src/main/java/hudson/remoting/ClassFilter.java
@@ -81,8 +81,8 @@ public abstract class ClassFilter {
      * values provide for user specified overrides - see {@link #FILE_OVERRIDE_LOCATION_PROPERTY}.
      */
     /*package*/ static ClassFilter createDefaultInstance() {
-        List<Pattern> patternOverride = loadPatternOverride();
         try {
+            List<Pattern> patternOverride = loadPatternOverride();
             if (patternOverride != null) {
                 LOGGER.log(Level.FINE, "Using user specified overrides for class blacklisting");
                 return new RegExpClassFilter(patternOverride);
