fc001975e029d08ea73342339364864354eddd6a
Stephen Connolly <stephen.alan.connolly@gmail.com>
2016-07-22 11:18:40 +0100

    [JENKINS-36871] Open source the utility classes

---
+++ src/main/java/org/jenkinsci/remoting/util/ByteBufferQueue.java
@@ -0,0 +1,359 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.remoting.util;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import javax.annotation.concurrent.NotThreadSafe;
+
+/**
+ * A helper class to make queuing up of data easier.
+ *
+ * @since FIXME
+ */
+@NotThreadSafe
+public class ByteBufferQueue {
+    /**
+     * The initial size of the {@link #buffers} array.
+     */
+    private static final int INITIAL_CAPACITY = 16;
+    /**
+     * The size above which to consider shrinking the {@link #buffers} array after compaction.
+     */
+    private static final int SHRINK_CAPACITY = 512;
+    /**
+     * If, after this number of times where the shrink condition has been met without needing to grow the backing array,
+     * we will shrink the backing array.
+     */
+    private static final int SHRINK_THRESHOLD = 8;
+    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+    /**
+     * The array of buffers.
+     */
+    private ByteBuffer[] buffers;
+    /**
+     * The size of the individual {@link ByteBuffer} allocations.
+     */
+    private final int bufferSize;
+    /**
+     * Our current read index.
+     */
+    private int readIndex;
+    /**
+     * Our current write index.
+     */
+    private int writeIndex;
+    /**
+     * The number of times we could have shrunk.
+     */
+    private int shrinkCount;
+
+    /**
+     * Constructor.
+     *
+     * @param bufferSize the size of buffers to use for queuing.
+     */
+    public ByteBufferQueue(int bufferSize) {
+        if (bufferSize <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.buffers = new ByteBuffer[INITIAL_CAPACITY];
+        this.bufferSize = bufferSize;
+        this.readIndex = 0;
+        this.writeIndex = 0;
+        this.buffers[writeIndex] = ByteBuffer.allocate(bufferSize);
+    }
+
+    /**
+     * Compact the {@link #buffers} array so that the readIndex is {@literal 0}.
+     */
+    private void compact() {
+        // skip over any empty buffers
+        while (readIndex < writeIndex && buffers[readIndex].position() == 0) {
+            readIndex++;
+        }
+        if (readIndex > 0) {
+            // compact the array start
+            System.arraycopy(buffers, readIndex, buffers, 0, writeIndex - readIndex + 1);
+            writeIndex -= readIndex;
+            readIndex = 0;
+            Arrays.fill(buffers, writeIndex + 1, buffers.length, null);
+        }
+    }
+
+    /**
+     * Add a new write buffer.
+     */
+    private void addWriteBuffer() {
+        if (writeIndex + 1 >= buffers.length) {
+            compact();
+            if (writeIndex + 1 >= buffers.length) {
+                // we need to grow the backing array
+                buffers = Arrays.copyOf(buffers, buffers.length * 2);
+                shrinkCount = 0;
+            } else if (buffers.length >= SHRINK_CAPACITY && writeIndex + 1 < buffers.length / 4) {
+                // after compaction, writeIndex is less than 1/4 of the way through the backing array
+                // if we halve the backing array size, we still have room
+                shrinkCount++;
+                if (shrinkCount > SHRINK_THRESHOLD) {
+                    buffers = Arrays.copyOf(buffers, buffers.length / 2);
+                    shrinkCount = 0;
+                }
+            } else if (writeIndex + 1 >= buffers.length * 3 / 4) {
+                // after compaction, writeIndex is more than 3/4 of the way through the backing array
+                // reset the shrinkCount as we are using a sizeable amount of the backing array.
+                shrinkCount = 0;
+            }
+        }
+        writeIndex++;
+        if (buffers[writeIndex] == null) {
+            buffers[writeIndex] = newByteBuffer();
+        } else {
+            buffers[writeIndex].clear();
+        }
+    }
+
+    /**
+     * Creates a new byte buffer matched to the queue's current buffer size.
+     *
+     * @return a new byte buffer matched to the queue's current buffer size.
+     */
+    public ByteBuffer newByteBuffer() {
+        return ByteBuffer.allocate(bufferSize);
+    }
+
+    /**
+     * This method transfers the bytes remaining in the given source
+     * buffer appended onto this buffer queue.
+     *
+     * @param src The source buffer from which bytes are to be read.
+     */
+    public void put(ByteBuffer src) {
+        while (src.hasRemaining()) {
+            while (!buffers[writeIndex].hasRemaining()) {
+                addWriteBuffer();
+            }
+            if (src.remaining() > buffers[writeIndex].remaining()) {
+                int limit = src.limit();
+                src.limit(src.position() + buffers[writeIndex].remaining());
+                buffers[writeIndex].put(src);
+                src.limit(limit);
+            } else {
+                buffers[writeIndex].put(src);
+            }
+        }
+    }
+
+    /**
+     * Tells whether there are any bytes between the current read index and
+     * the write index.
+     *
+     * @return <tt>true</tt> if, and only if, there is at least one byte
+     * remaining in this buffer queue.
+     */
+    public boolean hasRemaining() {
+        for (int i = readIndex; i <= writeIndex; i++) {
+            if (buffers[i].position() > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Tells whether there are any bytes between the current read index and
+     * the write index.
+     *
+     * @param len the number of bytes that we want at least remaining
+     * @return <tt>true</tt> if, and only if, there is at least {@literal len} bytes
+     * remaining in this buffer queue.
+     */
+    public boolean hasRemaining(int len) {
+        for (int i = readIndex; i <= writeIndex; i++) {
+            len -= buffers[i].position();
+            if (len <= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void peek(ByteBuffer dst) {
+        int readIndex = this.readIndex;
+        while (dst.hasRemaining()) {
+            if (readIndex >= writeIndex && buffers[readIndex].position() == 0) {
+                break;
+            }
+            int p = buffers[readIndex].position();
+            int l = buffers[readIndex].limit();
+            try {
+                buffers[readIndex].position(0);
+                buffers[readIndex].limit(p);
+                if (buffers[readIndex].remaining() > dst.remaining()) {
+                    buffers[readIndex].limit(dst.remaining());
+                    dst.put(buffers[readIndex]);
+                    break;
+                } else {
+                    dst.put(buffers[readIndex]);
+                }
+            } finally {
+                buffers[readIndex].limit(l);
+                buffers[readIndex].position(p);
+            }
+            readIndex++;
+        }
+    }
+
+    /**
+     * This method transfers bytes from the head of this buffer queue into the given destination buffer.
+     * The number of bytes transferred will be the smaller of the number of bytes available and the remaining capacity
+     * of the destination buffer.
+     *
+     * @param dst the destination buffer into which bytes are to be written.
+     */
+    public void get(ByteBuffer dst) {
+        while (dst.hasRemaining()) {
+            if (readIndex >= writeIndex && buffers[readIndex].position() == 0) {
+                if (writeIndex > 0) {
+                    // this is a cheap compact
+                    buffers[0] = buffers[writeIndex];
+                    buffers[writeIndex] = null;
+                    readIndex = writeIndex = 0;
+                }
+                break;
+            }
+            buffers[readIndex].flip();
+            if (buffers[readIndex].remaining() > dst.remaining()) {
+                int limit = buffers[readIndex].limit();
+                buffers[readIndex].limit(buffers[readIndex].position() + dst.remaining());
+                dst.put(buffers[readIndex]);
+                buffers[readIndex].limit(limit);
+                buffers[readIndex].compact();
+                break;
+            } else {
+                dst.put(buffers[readIndex]);
+                if (readIndex < writeIndex) {
+                    buffers[readIndex++] = null;
+                } else {
+                    assert readIndex == writeIndex;
+                    buffers[readIndex].clear();
+                }
+            }
+        }
+    }
+
+    /**
+     * This method transfers the bytes remaining in the given source buffer inserted at the head of this buffer queue.
+     *
+     * @param src The source buffer from which bytes are to be read.
+     */
+    public void unget(ByteBuffer src) {
+        if (!src.hasRemaining()) {
+            return;
+        }
+        ByteBuffer[] inject = new ByteBuffer[src.remaining() / bufferSize + 1];
+        int injectIndex = 0;
+        while (src.hasRemaining()) {
+            if (src.remaining() > bufferSize) {
+                int limit = src.limit();
+                src.limit(src.position() + bufferSize);
+                inject[injectIndex++] = newByteBuffer().put(src);
+                src.limit(limit);
+            } else {
+                inject[injectIndex++] = newByteBuffer().put(src);
+            }
+        }
+        // we could try and cram the data into the current readIndex buffer, instead we will just put the data
+        // into a new buffer that we insert before readIndex
+        if (readIndex < injectIndex) {
+            int injectCount = injectIndex - readIndex;
+            while (writeIndex + injectCount >= buffers.length) {
+                buffers = Arrays.copyOf(buffers, buffers.length * 2);
+                shrinkCount = 0;
+            }
+            System.arraycopy(buffers, readIndex, buffers, injectIndex, writeIndex - readIndex + 1);
+            writeIndex += injectCount;
+            readIndex = 0;
+            System.arraycopy(inject, 0, buffers, readIndex, injectIndex);
+        } else {
+            readIndex -= injectIndex;
+            System.arraycopy(inject, 0, buffers, readIndex, injectIndex);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getName());
+        sb.append("[hasRemaining=");
+        sb.append(hasRemaining());
+        sb.append(",readIndex=");
+        sb.append(readIndex);
+        sb.append(",writeIndex=");
+        sb.append(writeIndex);
+        sb.append(",capacity=");
+        sb.append(buffers.length);
+        sb.append(",bufSize=");
+        sb.append(bufferSize);
+        sb.append(']');
+        return sb.toString();
+    }
+
+    /**
+     * Transfers all the bytes in the {@link ByteBufferQueue} from the read position to the write position into a new
+     * {@code byte[]}.
+     * @return the {@code byte[]}
+     */
+    public byte[] toByteArray() {
+        if (readIndex == writeIndex) {
+            if (buffers[readIndex].position() == 0) {
+                return EMPTY_BYTE_ARRAY;
+            }
+            buffers[readIndex].flip();
+            byte[] result = new byte[buffers[readIndex].remaining()];
+            buffers[readIndex].get(result);
+            buffers[readIndex].clear();
+            readIndex = writeIndex = 0;
+            return result;
+        }
+        int size = 0;
+        for (int index = readIndex; index <= writeIndex; index++) {
+            size += buffers[index].position();
+        }
+        byte[] result = new byte[size];
+        int pos = 0;
+        for (int index = readIndex; index <= writeIndex; index++) {
+            buffers[index].flip();
+            int count = buffers[index].remaining();
+            buffers[index].get(result, pos, count);
+            buffers[index].clear();
+            pos+=count;
+        }
+        readIndex = writeIndex = 0;
+        return result;
+    }
+}
