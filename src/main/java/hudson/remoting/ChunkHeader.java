2923f11ad151a909ce9c6f43575ae5c65834fe35
Stephen Connolly <stephen.alan.connolly@gmail.com>
2016-07-25 12:04:53 +0100

    [JENKINS-36871] A ByteBuffer backed CommandTransport

--- src/main/java/hudson/remoting/ChunkHeader.java
+++ src/main/java/hudson/remoting/ChunkHeader.java
@@ -1,5 +1,8 @@
 package hudson.remoting;
 
+import java.nio.ByteBuffer;
+import org.jenkinsci.remoting.util.ByteBufferQueue;
+
 /**
  * Parsing of the chunk header.
  *
@@ -11,6 +14,18 @@ package hudson.remoting;
  * @author Kohsuke Kawaguchi
  */
 public class ChunkHeader {
+    public static int read(ByteBuffer buf) {
+        return parse(buf.get(), buf.get());
+    }
+
+    public static int peek(ByteBuffer buf) {
+        return peek(buf, 0);
+    }
+
+    public static int peek(ByteBuffer buf, int pos) {
+        return parse(buf.get(pos), buf.get(pos + 1));
+    }
+
     public static int parse(byte[] buf) {
         return parse(buf, 0);
     }
@@ -29,6 +44,16 @@ public class ChunkHeader {
 
     public static int length(int header) {
         return header&0x7FFF;
+    }
+
+    public static void write(ByteBuffer buf, int length, boolean hasMore) {
+        buf.put((byte) ((hasMore ? 0x80 : 0) | (length >> 8)));
+        buf.put((byte) length);
+    }
+
+    public static void write(ByteBufferQueue buf, int length, boolean hasMore) {
+        buf.put((byte) ((hasMore ? 0x80 : 0) | (length >> 8)));
+        buf.put((byte) length);
     }
 
     public static byte[] pack(int length, boolean hasMore) {
