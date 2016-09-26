43923ac74a158a186f530dcd150a4e69df28591e
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-09-26 23:31:13 +0200

    Exceptional case in setLastModifiedTime

--- src/main/java/hudson/remoting/FileSystemJarCache.java
+++ src/main/java/hudson/remoting/FileSystemJarCache.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.attribute.FileTime;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -50,9 +52,12 @@ public class FileSystemJarCache extends JarCacheSupport {
         File jar = map(sum1, sum2);
         if (jar.exists()) {
             LOGGER.log(Level.FINER, String.format("Jar file cache hit %16X%16X",sum1,sum2));
-            if (touch)  jar.setLastModified(System.currentTimeMillis());
-            if (notified.add(new Checksum(sum1,sum2)))
+            if (touch)  {
+                Files.setLastModifiedTime(jar.toPath(), FileTime.fromMillis(System.currentTimeMillis()));
+            }
+            if (notified.add(new Checksum(sum1,sum2))) {
                 getJarLoader(channel).notifyJarPresence(sum1,sum2);
+            }
             return jar.toURI().toURL();
         }
         return null;
