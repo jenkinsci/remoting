d496898ab02ddec7974a8456c51257bf2d9c82cc
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-07-20 14:02:24 +0300

    [JENKINS-45349] - Allow disabling file caching in HelloGetResource and HelloGetResources.
    This change adds an optional parameter, which disables file caching. My plan is to reuse it in Remoting tests in order to suppress some issues in Windows tests.

--- src/main/java/test/HelloGetResources.java
+++ src/main/java/test/HelloGetResources.java
@@ -4,21 +4,51 @@ import hudson.remoting.Callable;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Enumeration;
 
 /**
+ * Lists all resources as a single string.
  * @author Kohsuke Kawaguchi
  */
 public class HelloGetResources implements Callable<String,IOException> {
+
+    final boolean useCaches;
+    
+    @Deprecated
+    public HelloGetResources() {
+        this(true);
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param useCaches If {@code false}, caching in the {@link URLConnection} will be disabled.
+     *                  A default value will be used otherwise.
+     * @since 1.1
+     */
+    public HelloGetResources(boolean useCaches) {
+        this.useCaches = useCaches;
+    }
+    
+    @Override
     public String call() throws IOException {
         Enumeration<URL> e = getClass().getClassLoader().getResources("test/hello.txt");
         StringBuilder b = new StringBuilder();
         while (e.hasMoreElements()) {
             URL u = e.nextElement();
+            URLConnection connection = u.openConnection();
+            if (!useCaches) {
+                connection.setUseCaches(false);
+            }
+            
             b.append(u);
             b.append("::");
-            b.append(IOUtils.toString(u));
+            try (InputStream istream = connection.getInputStream()) {
+                b.append(IOUtils.toString(istream));
+            }
             b.append('\n');
         }
         return b.toString();
