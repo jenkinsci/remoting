f999f5567e696b19db4b4b66cc71d4004344393b
Rebecca Ysteboe <rysteboe@users.noreply.github.com>
2017-10-05 10:44:14 -0400

    [JENKINS-38711] Add UncaughtExceptionHandlers to the various remoting threads (#187)
    * [JENKINS-38711] Add UncaughtExceptionHandlers to the various remoting threads
    
    * [JENKINS-38711] Add handling to missed threads
    
    * [JENKINS-38711] Fix thread restart bug
    
    * [JENKINS-38711] Remove test exception
    
    * [JENKINS-38711] Move stream closures outside of CopyThread
    
    * [JENKINS-38711] Fix log levels
    
    * [JENKINS-38711] Fix CopyThread input bug
    
    * [JENKINS-38711] Don't close provided OutputStream in ForwarderFactory

--- src/main/java/hudson/remoting/DaemonThreadFactory.java
+++ src/main/java/hudson/remoting/DaemonThreadFactory.java
@@ -1,14 +1,19 @@
 package hudson.remoting;
 
 import java.util.concurrent.ThreadFactory;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 public class DaemonThreadFactory implements ThreadFactory {
+    private static final Logger LOGGER = Logger.getLogger(DaemonThreadFactory.class.getName());
+
     public Thread newThread(Runnable r) {
-        Thread t = new Thread(r);
-        t.setDaemon(true);
-        return t;
+        Thread thread = new Thread(r);
+        thread.setDaemon(true);
+        thread.setUncaughtExceptionHandler((t, e) -> LOGGER.log(Level.SEVERE, "Unhandled exception in thread " + t, e));
+        return thread;
     }
 }
