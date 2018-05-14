adc0df4ff783938442890f2c65bc0105ce60da96
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-05-14 18:04:36 +0200

    [JENKINS-50933] - Update patch for JENKINS-51302, remork the manual flow to Makefile

--- src/test/it/manualCheck/run_pct.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-docker run --rm -v maven-repo:/root/.m2 -v $(pwd)/out:/pct/out \
-    -v $(pwd)/target/custom-war-packager-maven-plugin/output/target/jenkins-remoting-it-1.0-remoting-it-SNAPSHOT.war:/pct/jenkins.war:ro \
-    -e ARTIFACT_ID=windows-slaves \
-    -e INSTALL_BUNDLED_SNAPSHOTS=true \
-    jenkins/pct
