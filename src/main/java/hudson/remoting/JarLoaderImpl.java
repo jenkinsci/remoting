a870ae09de2299b379da04e1f9c07d4d8cdbbe6c
Jesse Glick <jglick@cloudbees.com>
2018-09-12 19:25:29 -0400

    Channel.notifyJar was being called too often.

--- src/main/java/hudson/remoting/JarLoaderImpl.java
+++ src/main/java/hudson/remoting/JarLoaderImpl.java
@@ -6,12 +6,15 @@ import java.io.File;
 import java.io.IOException;
 import java.io.NotSerializableException;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Implements {@link JarLoader} to be called from the other side.
@@ -20,6 +23,9 @@ import java.util.concurrent.ConcurrentMap;
  */
 @edu.umd.cs.findbugs.annotations.SuppressWarnings("SE_BAD_FIELD")
 class JarLoaderImpl implements JarLoader, SerializableOnlyOverRemoting {
+
+    private static final Logger LOGGER = Logger.getLogger(JarLoaderImpl.class.getName());
+
     private final ConcurrentMap<Checksum,URL> knownJars = new ConcurrentHashMap<>();
 
     @edu.umd.cs.findbugs.annotations.SuppressWarnings("DMI_COLLECTION_OF_URLS") // TODO: fix this
@@ -33,6 +39,20 @@ class JarLoaderImpl implements JarLoader, SerializableOnlyOverRemoting {
         if (url==null)
             throw new IOException("Unadvertised jar file "+k);
 
+        Channel channel = Channel.current();
+        if (channel != null) {
+            if (url.getProtocol().equals("file")) {
+                try {
+                    channel.notifyJar(new File(url.toURI()));
+                } catch (URISyntaxException | IllegalArgumentException x) {
+                    LOGGER.log(Level.WARNING, "cannot properly report " + url, x);
+                }
+            } else {
+                LOGGER.log(Level.FINE, "serving non-file URL {0}", url);
+            }
+        } else {
+            LOGGER.log(Level.WARNING, "no active channel");
+        }
         Util.copy(url.openStream(), sink);
         presentOnRemote.add(k);
     }
