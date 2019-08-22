0e76c78d8a7f8c0d74fefb31d6e56cf4649a6702
Raihaan Shouhell <raihaan.shouhell@autodesk.com>
2019-08-22 19:06:26 +0800

    Remove all the remaining code

--- src/main/java/hudson/remoting/Base64.java
+++ src/main/java/hudson/remoting/Base64.java
@@ -15,8 +15,6 @@
  */
 package hudson.remoting;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
 
@@ -40,60 +38,6 @@ import javax.annotation.Nullable;
 @Deprecated
 public final class  Base64 {
 
-    static private final int  BASELENGTH         = 128;
-    static private final int  LOOKUPLENGTH       = 64;
-    static private final char PAD                = '=';
-    static final private byte [] base64Alphabet        = new byte[BASELENGTH];
-    static final private char [] lookUpBase64Alphabet  = new char[LOOKUPLENGTH];
-
-    static {
-
-        for (int i = 0; i < BASELENGTH; ++i) {
-            base64Alphabet[i] = -1;
-        }
-        for (int i = 'Z'; i >= 'A'; i--) {
-            base64Alphabet[i] = (byte) (i-'A');
-        }
-        for (int i = 'z'; i>= 'a'; i--) {
-            base64Alphabet[i] = (byte) ( i-'a' + 26);
-        }
-
-        for (int i = '9'; i >= '0'; i--) {
-            base64Alphabet[i] = (byte) (i-'0' + 52);
-        }
-
-        base64Alphabet['+']  = 62;
-        base64Alphabet['/']  = 63;
-
-        for (int i = 0; i<=25; i++)
-            lookUpBase64Alphabet[i] = (char)('A'+i);
-
-        for (int i = 26,  j = 0; i<=51; i++, j++)
-            lookUpBase64Alphabet[i] = (char)('a'+ j);
-
-        for (int i = 52,  j = 0; i<=61; i++, j++)
-            lookUpBase64Alphabet[i] = (char)('0' + j);
-        lookUpBase64Alphabet[62] = (char)'+';
-        lookUpBase64Alphabet[63] = (char)'/';
-
-    }
-
-    protected static boolean isWhiteSpace(char octect) {
-        return (octect == 0x20 || octect == 0xd || octect == 0xa || octect == 0x9);
-    }
-
-    protected static boolean isPad(char octect) {
-        return (octect == PAD);
-    }
-
-    protected static boolean isData(char octect) {
-        return (octect < BASELENGTH && base64Alphabet[octect] != -1);
-    }
-
-    protected static boolean isBase64(char octect) {
-        return (isWhiteSpace(octect) || isPad(octect) || isData(octect));
-    }
-
     /**
      * Encodes hex octects into Base64
      *
@@ -116,33 +60,11 @@ public final class  Base64 {
      * @return Array containing decoded data. {@code null} if the data cannot be decoded.
      */
     @CheckForNull
-    @SuppressFBWarnings(value = "PZLA_PREFER_ZERO_LENGTH_ARRAYS",
-            justification = "Null arrays are the part of the library API")
     public static byte[] decode(String encoded) {
 
         if (encoded == null)
             return null;
 
         return java.util.Base64.getDecoder().decode(encoded);
-    }
-
-    /**
-     * remove WhiteSpace from MIME containing encoded Base64 data.
-     *
-     * @param data  the byte array of base64 data (with WS)
-     * @return      the new length
-     */
-    protected static int removeWhiteSpace(char[] data) {
-        if (data == null)
-            return 0;
-
-        // count characters that's not whitespace
-        int newSize = 0;
-        int len = data.length;
-        for (int i = 0; i < len; i++) {
-            if (!isWhiteSpace(data[i]))
-                data[newSize++] = data[i];
-        }
-        return newSize;
     }
 }
