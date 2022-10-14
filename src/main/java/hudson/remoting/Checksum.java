1e97fc75733d27d9b3ca48963ced92cc67531556
Vincent Latombe <vincent@latombe.net>
2022-10-14 09:42:20 +0200

    Unused import

--- src/main/java/hudson/remoting/Checksum.java
+++ src/main/java/hudson/remoting/Checksum.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.Serializable;
 import java.net.URL;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
