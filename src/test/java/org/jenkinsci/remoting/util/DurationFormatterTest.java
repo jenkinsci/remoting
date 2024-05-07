bae4f1d389cb07464901ed249d5e8b26e0d1488e
Vincent Latombe <vincent@latombe.net>
2024-05-07 16:36:29 +0200

    Fix tests

--- src/test/java/org/jenkinsci/remoting/util/DurationFormatterTest.java
+++ src/test/java/org/jenkinsci/remoting/util/DurationFormatterTest.java
@@ -11,8 +11,8 @@ public class DurationFormatterTest {
         assertEquals("1 second", DurationFormatter.format(Duration.ofSeconds(1)));
         assertEquals("2 seconds", DurationFormatter.format(Duration.ofSeconds(2)));
         assertEquals("1 day, 2 seconds", DurationFormatter.format(Duration.ofDays(1).plus(Duration.ofSeconds(2))));
-        assertEquals("1 day, 3 hours, 2 seconds", DurationFormatter.format(Duration.ofDays(2).plus(Duration.ofHours(3)).plus(Duration.ofSeconds(2))));
-        assertEquals("1 day, 3 hours, 1 minute, 2 seconds", DurationFormatter.format(
+        assertEquals("2 days, 3 hours, 2 seconds", DurationFormatter.format(Duration.ofDays(2).plus(Duration.ofHours(3)).plus(Duration.ofSeconds(2))));
+        assertEquals("2 days, 3 hours, 1 minute, 2 seconds", DurationFormatter.format(
                 Duration.ofDays(2)
                 .plus(Duration.ofHours(3))
                 .plus(Duration.ofMinutes(1))
